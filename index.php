b0ec4b48ce5b838a2f0e808a7198b16d736e93fa
Saja Jaya <kadeksajajaya23@gmail.com>
2025-12-01 16:06:44 +0800

    memperbaiki nama file yang di link di index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $stmt = $buku->readAll();
 <head><title>Daftar Buku</title></head>
 <body>
     <h1>Perpustakaan: Daftar Buku</h1>
-    <a href="tambah.php">+ Tambah Buku</a>
+    <a href="create.php">+ Tambah Buku</a>
     <table border="1" cellpadding="10" style="margin-top:10px; border-collapse:collapse; width:100%;">
         <thead>
             <tr style="background:#eee;">
@@ -30,7 +30,7 @@ $stmt = $buku->readAll();
                 <td><?= $row['genre'] ?></td>
                 <td>
                     <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> | 
-                    <a href="hapus.php?id=<?= $row['id'] ?>" onclick="return confirm('Hapus?')">Hapus</a>
+                    <a href="delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Hapus?')">Hapus</a>
                 </td>
             </tr>
             <?php endwhile; ?>
